608c8bd97da879a6e66e62eb5df9db81ce6f6b72
mamasb <pkpipimwangi@gmail.com>
2024-05-25 03:52:28 +0300

    l remove any carriage return characters (CR) and ensure that the file uses Unix line endings (LF).

---
+++ base.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+import uuid
+
+class Base:
+    def __init__(self, id=None, **kwargs):
+        if not id:
+            self.id = uuid.uuid4().hex
+        else:
+            self.id = id
+
+        for key, value in kwargs.items():
+            self.__dict__[key] = value
+
+    def __str__(self):
+        return f"The id of the object is {self.id}"
+
+personDict = {"name": "Philip", "field": "SE"}
+base1 = Base(**personDict)
+base2 = Base('Ola')
+
+print(base1, base2)
+print(base1.__dict__, base2.__dict__)
